5a56bb7496610762c20c18986b69baa9538ea87e
MohammedGouaouri <im_gouaouri@esi.dz>
2023-02-04 20:19:04 +0100

    train sbert classifier started code added

---
+++ run_sbert_classifier.py
@@ -0,0 +1,274 @@
+from __future__ import absolute_import, division, print_function
+
+
+import argparse
+import csv
+import logging
+import os
+import random
+import sys
+import json
+import numpy as np
+import torch
+from torch import nn
+from sentence_transformers import InputExample
+from sentence_transformers import models, SentenceTransformer
+from sentence_transformers import losses
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+
+MODEL_TO_SAVE = "skgbert-cls"
+
+
+class KGInputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, text_c=None, label=None):
+        """Constructs a KGInputExample.
+
+        Args:
+            guid: Unique id for the example.
+            text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            text_c: (Optional) string. The untokenized text of the third sequence.
+            Only must be specified for sequence triple tasks.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.text_c = text_c
+        self.label = label
+
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
+
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `KGInputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `KGInputExample`s for the dev set."""
+        raise NotImplementedError()
+
+    def get_labels(self, data_dir):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with open(input_file, "r", encoding="utf-8") as f:
+            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+            lines = []
+            for line in reader:
+                if sys.version_info[0] == 2:
+                    line = list(unicode(cell, 'utf-8') for cell in line)
+                lines.append(line)
+            return lines
+
+class KGProcessor(DataProcessor):
+    """Processor for knowledge graph data set."""
+
+    def __init__(self):
+        self.labels = set()
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train", data_dir)
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev", data_dir)
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test", data_dir)
+
+    def get_relations(self, data_dir):
+        """Gets all labels (relations) in the knowledge graph."""
+        # return list(self.labels)
+        with open(os.path.join(data_dir, "relations.txt"), 'r') as f:
+            lines = f.readlines()
+            relations = []
+            for line in lines:
+                relations.append(line.strip())
+        return relations
+
+    def get_labels(self, data_dir):
+        """Gets all labels (0, 1) for triples in the knowledge graph."""
+        return ["0", "1"]
+
+    def get_entities(self, data_dir):
+        """Gets all entities in the knowledge graph."""
+        # return list(self.labels)
+        with open(os.path.join(data_dir, "entities.txt"), 'r') as f:
+            lines = f.readlines()
+            entities = []
+            for line in lines:
+                entities.append(line.strip())
+        return entities
+
+    def get_train_triples(self, data_dir):
+        """Gets training triples."""
+        return self._read_tsv(os.path.join(data_dir, "train.tsv"))
+
+    def get_dev_triples(self, data_dir):
+        """Gets validation triples."""
+        return self._read_tsv(os.path.join(data_dir, "dev.tsv"))
+
+    def get_test_triples(self, data_dir):
+        """Gets test triples."""
+        return self._read_tsv(os.path.join(data_dir, "test.tsv"))
+
+    def _create_examples(self, lines, set_type, data_dir):
+        """Creates examples for the training and dev sets."""
+        # entity to text
+        ent2text = {}
+        with open(os.path.join(data_dir, "entity2text.txt"), 'r') as f:
+            ent_lines = f.readlines()
+            for line in ent_lines:
+                temp = line.strip().split('\t')
+                if len(temp) == 2:
+                    ent2text[temp[0]] = temp[1]
+
+        entities = list(ent2text.keys())
+
+        rel2text = {}
+        with open(os.path.join(data_dir, "relation2text.txt"), 'r') as f:
+            rel_lines = f.readlines()
+            for line in rel_lines:
+                temp = line.strip().split('\t')
+                rel2text[temp[0]] = temp[1]
+
+        lines_str_set = set(['\t'.join(line) for line in lines])
+        examples = []
+        for (i, line) in enumerate(lines):
+
+            head_ent_text = ent2text[line[0]]
+            tail_ent_text = ent2text[line[2]]
+            relation_text = rel2text[line[1]]
+
+            if set_type == "dev" or set_type == "test":
+                triple_label = line[3]
+                if triple_label == "1":
+                    label = "1"
+                else:
+                    label = "0"
+
+                guid = "%s-%s" % (set_type, i)
+                text_a = head_ent_text
+                text_b = relation_text
+                text_c = tail_ent_text
+                self.labels.add(label)
+                examples.append(
+                    KGInputExample(guid=guid, text_a=text_a, text_b=text_b, text_c=text_c, label=label))
+
+            elif set_type == "train":
+                guid = "%s-%s" % (set_type, i)
+                text_a = head_ent_text
+                text_b = relation_text
+                text_c = tail_ent_text
+                examples.append(
+                    KGInputExample(guid=guid, text_a=text_a, text_b=text_b, text_c=text_c, label="1"))
+
+                rnd = random.random()
+                guid = "%s-%s" % (set_type + "_corrupt", i)
+                if rnd <= 0.5:
+                    # corrupting head
+                    tmp_head = ''
+                    while True:
+                        tmp_ent_list = set(entities)
+                        tmp_ent_list.remove(line[0])
+                        tmp_ent_list = list(tmp_ent_list)
+                        tmp_head = random.choice(tmp_ent_list)
+                        tmp_triple_str = tmp_head + \
+                            '\t' + line[1] + '\t' + line[2]
+                        if tmp_triple_str not in lines_str_set:
+                            break
+                    tmp_head_text = ent2text[tmp_head]
+                    examples.append(
+                        KGInputExample(guid=guid, text_a=tmp_head_text, text_b=text_b, text_c=text_c, label="0"))
+                else:
+                    # corrupting tail
+                    tmp_tail = ''
+                    while True:
+                        tmp_ent_list = set(entities)
+                        tmp_ent_list.remove(line[2])
+                        tmp_ent_list = list(tmp_ent_list)
+                        tmp_tail = random.choice(tmp_ent_list)
+                        tmp_triple_str = line[0] + \
+                            '\t' + line[1] + '\t' + tmp_tail
+                        if tmp_triple_str not in lines_str_set:
+                            break
+                    tmp_tail_text = ent2text[tmp_tail]
+                    examples.append(
+                        KGInputExample(guid=guid, text_a=text_a, text_b=text_b, text_c=tmp_tail_text, label="0"))
+        return examples
+
+
+
+
+logger = logging.getLogger(__name__)
+
+data_dir="./data/umls"
+batch_size = 16
+
+processor = KGProcessor()
+
+
+print("********** Preparing training dataset **********")
+label_list = processor.get_labels(data_dir)
+entity_list = processor.get_entities(data_dir)
+## Train examples is an array of KGInputExamples objects
+## We can extract text_a, text_b and text_b 
+train_examples = processor.get_train_examples(data_dir)
+
+skgbert_train_examples = []
+for example in tqdm(train_examples):
+    skgbert_train_examples.append(
+        InputExample(
+            texts=[ example.text_a, example.text_b ,example.text_c ],
+            label=int(example.label)
+        )
+    )
+
+loader = DataLoader(
+    skgbert_train_examples, 
+    shuffle=True, 
+    batch_size=batch_size
+)
+
+
+print("********** Training The model *************")
+bert = models.Transformer('bert-base-uncased')
+pooler = models.Pooling(
+    bert.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True
+)
+model = SentenceTransformer(modules=[bert, pooler])
+
+
+loss = losses.SoftmaxLoss(
+    model=model,
+    sentence_embedding_dimension=model.get_sentence_embedding_dimension(),
+    num_labels=2)
+
+epochs = 5
+warmup_steps = int(len(loader) * epochs * 0.1)
+model.fit(
+    train_objectives=[(loader, loss)],
+    epochs=epochs,
+    warmup_steps=warmup_steps,
+    output_path=MODEL_TO_SAVE,
+    show_progress_bar=True,
+)
+
+print("********** Model finished training *************")
